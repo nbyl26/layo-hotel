8fc1465a951d94b7b2266472f1fe7142a8d9d5fa
Nabil <nabilpasha230606@gmail.com>
2024-11-26 22:57:18 +0700

    Setup basic project structure and routing

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div className="container text-center mt-5">
+      <h1>Welcome to Layo-Hotel</h1>
+      <p>Your comfort, our priority.</p>
+    </div>
+  );
+};
+
+export default Home;
