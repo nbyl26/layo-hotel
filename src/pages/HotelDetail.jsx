b086cc76022e19678d900a963dc0a87c4cdce954
Nabil <nabilpasha230606@gmail.com>
2024-11-26 23:06:09 +0700

    Added Hotel Detail Page

---
+++ src/pages/HotelDetail.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import Header from '../components/Header';
+import Footer from '../components/Footer';
+import HotelBanner from '../components/HotelBanner';
+import HotelDetails from '../components/HotelDetails';
+import CustomerReviews from '../components/CustomerReviews';
+import BookingForm from '../components/BookingForm';
+
+const HotelDetail = () => {
+  const hotel = {
+    name: 'Ocean View',
+    location: 'Lombok',
+    image: 'https://via.placeholder.com/600x300',
+    description: 'A luxurious seaside resort offering breathtaking ocean views.',
+    facilities: ['Swimming Pool', 'Free Wi-Fi', 'Spa & Wellness Center', 'Restaurant', 'Bar'],
+  };
+
+  const reviews = [
+    { name: 'Alice', comment: 'Great stay! Highly recommended.', rating: 5 },
+    { name: 'Bob', comment: 'Beautiful place, but service could be better.', rating: 4 },
+  ];
+
+  return (
+    <>
+      <Header />
+      <HotelBanner hotel={hotel} />
+      <HotelDetails hotel={hotel} />
+      <CustomerReviews reviews={reviews} />
+      <BookingForm />
+      <Footer />
+    </>
+  );
+};
+
+export default HotelDetail;
