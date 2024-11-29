a4da862bdb0fc155bba6d39db24d6d7914ebea84
Nabil <nabilpasha230606@gmail.com>
2024-11-29 22:42:30 +0700

    Improve on Service Components

--- src/components/ServiceComponent.jsx
+++ src/components/ServiceComponent.jsx
@@ -1,37 +1,39 @@
 import React from "react";
-import "../styles/service.css"; // File CSS untuk styling khusus komponen ini
+import "../styles/service.css"; 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUtensils, faSpa, faWaterLadder, faDumbbell, faSquarePhoneFlip, faBusinessTime} from "@fortawesome/free-solid-svg-icons";
 
 const ServiceComponent = () => {
     const services = [
         {
             name: "Restaurant",
             description: "Delicious meals served 24/7 with a variety of cuisines.",
-            icon: "bi bi-egg-fried",
+            icon: faUtensils,
         },
         {
             name: "Spa & Wellness",
             description: "Relax and rejuvenate with our spa and wellness services.",
-            icon: "bi bi-heart",
+            icon: faSpa,
         },
         {
             name: "Swimming Pool",
             description: "Enjoy a refreshing swim in our outdoor pool.",
-            icon: "bi bi-cloud-sun",
+            icon: faWaterLadder,
         },
         {
             name: "Fitness Center",
             description: "State-of-the-art gym facilities to keep you fit.",
-            icon: "bi bi-person-bounding-box",
+            icon: faDumbbell,
         },
         {
             name: "Room Service",
             description: "Order food and drinks to your room any time.",
-            icon: "bi bi-bag-check",
+            icon: faSquarePhoneFlip,
         },
         {
             name: "Conference Room",
             description: "Fully equipped conference room for business meetings.",
-            icon: "bi bi-people",
+            icon: faBusinessTime,
         },
     ];
 
@@ -43,7 +45,8 @@ const ServiceComponent = () => {
                 {services.map((service, index) => (
                     <div className="col-lg-4 col-md-6 mb-4" key={index}>
                         <div className="service-box">
-                            <i className={`service-icon ${service.icon}`}></i>
+                            {/* <i className={`service-icon ${service.icon}`}></i> */}
+                            <i className= "service-icon" ><FontAwesomeIcon icon={service.icon} /></i>
                             <h3 className="service-title">{service.name}</h3>
                             <p className="service-description">{service.description}</p>
                         </div>
